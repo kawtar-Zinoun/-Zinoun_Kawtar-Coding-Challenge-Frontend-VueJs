e03f2bb26f9cceaf985f6125a131713e549a2a1c
kawtar-Zinoun <kawtar.zinoun@usmba.ac.ma>
2022-02-21 00:30:47 +0100

    setup github OAuth with github backend V3 and Gatekeeper

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import router from './router'
+import VueRouter from 'vue-router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, axios)
+
+Vue.use(VueRouter)
+
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container">
+
+      <span class="title">
+        Front-End Coding Challenge
+      </span>
+
+      <div class="box">
+       <a href="https://github.com/login/oauth/authorize/?client_id=0b4c30e2607568994db1"> Authorize my Github Account </a>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default 
+{
+  name: 'Login',
+  
+  data() {
+    return {
+      info: null
+    }
+  },
+  
+  methods : {
+
+    getAccessToken(code) {     // request access token with gatekeeper api
+      this.axios
+      .get('http://localhost:9999/authenticate/'+code)   
+      .then(response => (console.log(response)))
+    }
+  },
+  
+  
+
+  mounted() { 
+      //                            fetching github code from url 
+      let myUrl = window.location.href;
+      let splitedUrl = myUrl.split('?');
+
+      // check if url has parameter
+      let code = (splitedUrl.length > 1 && splitedUrl[1] !== '') ? myUrl.match(/\?code=(.*)#/)[1] : null ; 
+      
+      if (code != null) this.getAccessToken(code);
+
+    
+  }
+}
+
+</script>
+
+
+<style>
+
+.container {
+  height:100vh;
+  min-width: 100vw;
+  font-family: 'Poppins', sans-serif;
+  background-color:#95389E;
+  }
+
+
+.title {
+  font-size: 30px;
+  width: 100vw;
+  text-align: center;
+  display: block;
+  font-weight: 600;
+  padding-top: 50px;
+  color: #fff;
+}
+
+.box {
+  width:30%;
+  height: 65px;
+  background-color: rgb(240, 217, 255, 0.2);
+  margin-top: 15%;
+  border-radius: 10px;
+  margin-right: auto;
+  margin-left: auto;
+  display: flex;
+  justify-content: center;
+}
+
+.box a {
+  color: #FBFBFB;
+  opacity: .8;
+  align-self: center;
+  text-decoration: none;
+}
+
+.box a:hover {
+  color: black;
+  cursor: pointer;
+}
+
+.box:hover {
+  background-color: rgb(240, 217, 255, 0.5);
+  cursor: pointer;
+}
+
+
+</style>
